f6b6e7ffeb024af66f668918fa8a5a6606ca964d
JanB <eneroboy1@gmail.com>
2023-12-31 10:10:39 +0100

    Revert "Testowanie daty"
    This reverts commit 8ddeb531ae40a77fb4aecc80bc75257df6aa4022.

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 
-require 'Routing.php';
-
-$path = trim($_SERVER['REQUEST_URI'], '/');
-$path = parse_url( $path, PHP_URL_PATH);
-
-Router::get('', 'DefaultController');
-Router::get('projects', 'DefaultController');
-
-Router::run($path);
+echo 'Hi there 👋';
+?>
